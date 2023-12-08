1e2fa7d516f8996a7a4ade785378225ecac2803a
Eric Schultz <eric@commitchange.com>
2023-12-07 19:37:44 -0600

    Get working typescript

--- app/javascript/bundles/HelloWorld/components/HelloWorld.tsx
+++ app/javascript/bundles/HelloWorld/components/HelloWorld.tsx
@@ -1,8 +1,7 @@
-import PropTypes from 'prop-types';
 import React, { useState } from 'react';
-import style from './HelloWorld.module.css';
+//import style from './HelloWorld.module.css';
 
-const HelloWorld = (props) => {
+const HelloWorld = (props: { name: string; }) => {
   const [name, setName] = useState(props.name);
 
   return (
@@ -10,17 +9,13 @@ const HelloWorld = (props) => {
       <h3>Hello, {name}!</h3>
       <hr />
       <form>
-        <label className={style.bright} htmlFor="name">
+        <label htmlFor="name">
           Say hello to:
           <input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
         </label>
       </form>
     </div>
   );
-};
-
-HelloWorld.propTypes = {
-  name: PropTypes.string.isRequired, // this is passed from the Rails view
 };
 
 export default HelloWorld;
