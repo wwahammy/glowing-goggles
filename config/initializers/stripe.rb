e5f65c771e26d16c46d3a8e9927d89739e25e519
Eric Schultz <eric@commitchange.com>
2023-12-08 19:31:16 -0600

    Run standardrb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,12 @@
+Stripe.api_key = ENV.fetch("STRIPE_API_KEY")
 
+StripeEvent.signing_secret = ENV["STRIPE_SIGNING_SECRET"]
 
-Stripe.api_key = ENV.fetch('STRIPE_API_KEY')
+StripeEvent.configure do |events|
+  events.subscribe "payment_intent." do |event|
+  end
+
+  events.all do |event|
+    Rails.logger.log(event)
+  end
+end
--- spec/factories/stripe_payment_intent_records.rb
+++ spec/factories/stripe_payment_intent_records.rb
@@ -1,5 +1,4 @@
 FactoryBot.define do
   factory :stripe_payment_intent_record do
-    
   end
 end
